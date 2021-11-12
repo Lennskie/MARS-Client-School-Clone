b1287d627bd286a04f9754e695ddaadf84e70450
Stef Wauters <stef.wauters@student.howest.be>
2021-11-12 10:39:35 +0100

    add styling to nav and basic layout

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,6 +2,7 @@ let config;
 let api;
 
 document.addEventListener("DOMContentLoaded", init);
+document.querySelector('#home-link').addEventListener("click", goHome);
 
 async function init() {
     config = await loadConfig();
@@ -15,3 +16,8 @@ async function loadConfig() {
     const response = await fetch("config.json");
     return response.json();
 }
+
+function goHome() {
+    window.location.href = "./index.html";
+}
+
