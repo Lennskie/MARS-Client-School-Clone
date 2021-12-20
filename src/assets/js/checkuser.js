1b80cc82fc0e7a1e7fc336a636d1603c47b7741b
lennc <lenn.crochart@student.howest.be>
2021-12-20 15:05:06 +0100

    Client, employee and unknown are all handled

--- src/assets/js/checkuser.js
+++ src/assets/js/checkuser.js
@@ -8,27 +8,52 @@ function init(){
     document.querySelector('h1').innerHTML = "Hello, " + auth;
     if(checkUser() === "client"){
         setTimeout( function(){
-            document.querySelector('.loader').className += " loader-done"
-            document.querySelector('.loader').classList.remove("loader")
+            document.querySelector("p").innerHTML = "Welcome to M.A.R.S, " + auth;
+            document.querySelector('.loader').className += " loader-done";
+            document.querySelector('.loader').classList.remove("loader");
             document.querySelector('.loader-done').innerHTML =
                 `<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
                       <circle class="checkmark__circle" cx="00" cy="00" r="55" fill="none"/>
                       <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
                 </svg>`
-        }, 2000)
+            setTimeout(function(){document.location.href = "support.html"}, 2000);
+        }, 2000);
+    }else if(checkUser() === "employee"){
+        setTimeout( function(){
+            document.querySelector("p").innerHTML = "Welcome to M.A.R.S, " + auth;
+            document.querySelector('.loader').className += " loader-done";
+            document.querySelector('.loader').classList.remove("loader");
+            document.querySelector('.loader-done').innerHTML =
+                `<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                      <circle class="checkmark__circle" cx="00" cy="00" r="55" fill="none"/>
+                      <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+                </svg>`
+            setTimeout(function(){document.location.href = "callpanel.html"}, 2000);
+        }, 2000);
+    }else if (checkUser() === false){
+        setTimeout( function() {
+            document.querySelector("p").innerHTML = "Seems like you're not from around here, " + auth;
+            document.querySelector('.loader').className += " loader-done";
+            document.querySelector('.loader').classList.remove("loader");
+            document.querySelector('.loader-done').innerHTML =
+                `<div class="crosssign"><p>X</p></div>`
+            setTimeout(function () { document.location.href = "login.html"}, 2000);
+        }, 2000);
     }
 }
 
 
 function checkUser(){
     if (auth === 'client'){
-        console.log("logged in as client");
         return "client";
     }else if (auth === 'employee'){
-        console.log("logged in as employee");
         return "employee";
     }else{
-        console.log("not a valid user");
-        return "invalid";
+        console.log("hey!")
+        return false;
     }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
