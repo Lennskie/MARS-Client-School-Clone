02a30ca1f65c7770910b4a4941660b17560b8518
lennc <lenn.crochart@student.howest.be>
2021-12-21 10:03:04 +0100

    Save me now button

--- src/assets/js/loginlogout.js
+++ src/assets/js/loginlogout.js
@@ -5,18 +5,25 @@ const auth = localStorage.getItem("auth")
 document.addEventListener("DOMContentLoaded", init);
 
 function init(){
-    document.querySelector("#btn-logout").addEventListener("click", logout)
+
+    document.querySelector("#btn-logout").addEventListener("click", logout);
+
+    //reset of all the hidden on pageload (this can be simplefied in a later stage)
     document.querySelector("#btn-logout").classList.remove("hidden");
     document.querySelector("#btn-login").classList.remove("hidden");
+    document.querySelector("#savemenow").classList.remove("hidden");
 
-    if (checkUser() !== false){
+    if (checkUser() !== false){ //you are logged in
         document.querySelector("#btn-login").classList.add("hidden");
-        document.querySelector("#subscribe").classList.remove("nav")
-        document.querySelector("#subscribe").classList.add("hidden")
-    }else{
-        document.querySelector("#btn-logout").classList.add("hidden")
-        document.querySelector("#subscribe").classList.add("nav")
-        document.querySelector("#subscribe").classList.remove("hidden")
+        document.querySelector("#savemenow").classList.add("hidden");
+        document.querySelector("#subscribe").classList.remove("nav");
+        document.querySelector("#subscribe").classList.add("hidden");
+
+    }else{ //you are not logged in
+        document.querySelector("#btn-logout").classList.add("hidden");
+        document.querySelector("#subscribe").classList.add("nav");
+        document.querySelector("#subscribe").classList.remove("hidden");
+        document.querySelector("#savemenow").classList.remove("hidden");
     }
 }
 
